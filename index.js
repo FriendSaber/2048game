2cfeaa3dd9d4481b1dfb8e712c9308f5aaa5f3b9
Seaborn Lee <seabornlee.cn@gmail.com>
2017-06-10 14:28:34 +0800

    feature: add draw random cell

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroun
 document.body.appendChild(app.view);
 
 var basicText = new PIXI.Text('2058', {
-    fontSize: 250
+    fontSize: 48
 });
 basicText.anchor.set(0.5);
 basicText.x = app.renderer.width/2;
@@ -19,8 +19,27 @@ for (var i = 0;i < 4;i ++) {
     for (var j = 0; j<4;j++) {
         var graphics = new PIXI.Graphics();
         graphics.beginFill(0xFF700B, 1);
-        graphics.drawRect(app.renderer.width / 8 + j * 155, app.renderer.height / 8 * 3 + i * 155, 150, 150);
+        graphics.drawRect(app.renderer.width / 8 + j * 77, app.renderer.height / 8 * 3 + i * 77, 75, 75);
         app.stage.addChild(graphics);
     }
 }
 
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 4);
+}
+
+var x = generateRandomNumber();
+var y = generateRandomNumber();
+
+var graphics = new PIXI.Graphics();
+graphics.beginFill(0xFF0000, 1);
+graphics.drawRect(app.renderer.width / 8 + x  * 77, app.renderer.height / 8 * 3 + y * 77, 75, 75);
+app.stage.addChild(graphics);
+
+var number = new PIXI.Text('2', {
+    fontSize: 48
+});
+number.anchor.set(0.5);
+number.x = 75/2 + app.renderer.width/ 8 + x * 77;
+number.y = 75 /2 + app.renderer.height / 8 * 3 + y * 77;
+app.stage.addChild(number);
